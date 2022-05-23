8d5f736fc06884c73457eaaff993ce50ea79e97b
andgarzonmal <andgarzonmal@unal.edu.co>
2022-05-23 17:15:21 -0500

    🪛 FIX bugs

--- script.js
+++ script.js
@@ -12,40 +12,48 @@ const book = ({ title, author, index }) => `
   <p>${title}</p> 
   <p>${author}</p> 
   <button class="removebook" id="removebook" value="Add Book" onclick="">Remove</button>
+  <hr>
 </li>`;
 
 function addbooks() {
   let mybook = { title: title.value, author: author.value, index: Date.now() };
   bookList.push(mybook);
-  // console.log(bookList);
   title.value = '';
   author.value = '';
 }
+
+function remove() {
+  const removebook = document.querySelectorAll('.removebook');
+  removebook.forEach((elem) => {
+    elem.addEventListener('click', () => {
+      elem.parentNode.remove();
+      console.log(bookList)
+      bookList = bookList.filter(book => book.index !== Number(elem.parentNode.id))
+      console.log(bookList);
+      localStorage.setItem('mylibrary', JSON.stringify(bookList)); 
+    });
+  });
+};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addbooks();
   addhtml();
+  remove();
 });
 
 function addhtml() {
   ullibrary.innerHTML = '';
   bookList.forEach((element) =>
     ullibrary.insertAdjacentHTML('beforeend', book(element))
-  );
+    );
   localStorage.setItem('mylibrary', JSON.stringify(bookList));
 }
+
 
 if (getmylibrary.length > 0) {
   bookList = getmylibrary;
   addhtml();
+  remove();
 }
 
-function remove() {
-  const removebook = document.getElementById('#removebook');
-  removebook.forEach((elem) => {
-    elem.addEventListener('click', () => {
-      elem.parentNode.remove();
-    });
-  });
-}
