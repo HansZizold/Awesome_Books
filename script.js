5c604130edebfd09e99c7c6c6795fa5f960f9a5a
Hans Zizold <hzizold@hotmail.com>
2022-05-23 17:00:27 -0400

    First Solution

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const bookList = [];
+let bookList = [];
 
 const library = document.querySelector(".library"); 
 const title = document.querySelector(".title"); 
 const author = document.querySelector(".author"); 
 const form = document.querySelector("#senddata");
+const ullibrary = document.querySelector(".ullibrary");
+const getmylibrary = JSON.parse(localStorage.getItem('mylibrary'));
 
 const book = ({title,author,index}) => `
 <li id=${index}>
@@ -15,10 +17,27 @@ const book = ({title,author,index}) => `
 function addbooks() {
   let mybook = {title: title.value, author: author.value, index: Date.now()};
   bookList.push(mybook);
-  console.log(bookList);
+  // console.log(bookList);
+  title.value = '';
+  author.value = '';
 }
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
   addbooks();
+  addhtml();
 });
+
+function addhtml() {
+  ullibrary.innerHTML='';
+  bookList.forEach(element=>ullibrary.insertAdjacentHTML('beforeend',book(element)));
+  localStorage.setItem('mylibrary', JSON.stringify(bookList));
+}
+
+if(getmylibrary.length > 0) {
+  bookList = getmylibrary;
+  addhtml();
+}
+
+
+
