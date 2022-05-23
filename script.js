96c3d9a28c3671c0ba4a337884b635ca7e1452f0
Shay Craig <71286979+craigs40@users.noreply.github.com>
2022-05-23 17:54:23 -0400

    Add remove function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let bookList = [];
 
-const library = document.querySelector(".library"); 
-const title = document.querySelector(".title"); 
-const author = document.querySelector(".author"); 
-const form = document.querySelector("#senddata");
-const ullibrary = document.querySelector(".ullibrary");
+const library = document.querySelector('.library');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const form = document.querySelector('#senddata');
+const ullibrary = document.querySelector('.ullibrary');
 const getmylibrary = JSON.parse(localStorage.getItem('mylibrary'));
 
-const book = ({title,author,index}) => `
+const book = ({ title, author, index }) => `
 <li id=${index}>
   <p>${title}</p> 
   <p>${author}</p> 
@@ -15,29 +15,37 @@ const book = ({title,author,index}) => `
 </li>`;
 
 function addbooks() {
-  let mybook = {title: title.value, author: author.value, index: Date.now()};
+  let mybook = { title: title.value, author: author.value, index: Date.now() };
   bookList.push(mybook);
   // console.log(bookList);
   title.value = '';
   author.value = '';
 }
 
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   addbooks();
   addhtml();
 });
 
 function addhtml() {
-  ullibrary.innerHTML='';
-  bookList.forEach(element=>ullibrary.insertAdjacentHTML('beforeend',book(element)));
+  ullibrary.innerHTML = '';
+  bookList.forEach((element) =>
+    ullibrary.insertAdjacentHTML('beforeend', book(element))
+  );
   localStorage.setItem('mylibrary', JSON.stringify(bookList));
 }
 
-if(getmylibrary.length > 0) {
+if (getmylibrary.length > 0) {
   bookList = getmylibrary;
   addhtml();
 }
 
-
-
+function remove() {
+  const removebook = document.getElementById('#removebook');
+  removebook.forEach((elem) => {
+    elem.addEventListener('click', () => {
+      elem.parentNode.remove();
+    });
+  });
+}
